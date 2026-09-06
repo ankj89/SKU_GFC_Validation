88ff118eaba5e7fd750d335e87aa6a91745417ba
ankj89 <51509357+ankj89@users.noreply.github.com>
2026-09-06 23:40:57 +0530

    Create drawingKnowledge.js

---
+++ drawingKnowledge.js
@@ -0,0 +1,37 @@
+// =====================================
+// DRAWING KNOWLEDGE ENGINE
+// =====================================
+
+let drawingKnowledge = {
+
+    skuMap: {},
+
+    roomPageMap: {}
+
+};
+
+// =====================================
+// RESET
+// =====================================
+
+function resetDrawingKnowledge(){
+
+    drawingKnowledge = {
+
+        skuMap:{},
+
+        roomPageMap:{}
+
+    };
+
+}
+
+// =====================================
+// GET
+// =====================================
+
+function getDrawingKnowledge(){
+
+    return drawingKnowledge;
+
+}

2bec20b1a782a7ceb9cf5386aa8786a5825cacde
ankj89 <51509357+ankj89@users.noreply.github.com>
2026-09-07 00:32:48 +0530

    Update drawingKnowledge.js

--- drawingKnowledge.js
+++ drawingKnowledge.js
@@ -4,9 +4,9 @@
 
 let drawingKnowledge = {
 
-    skuMap: {},
+    skuMap:{},
 
-    roomPageMap: {}
+    roomPageMap:{}
 
 };
 
@@ -16,7 +16,7 @@ let drawingKnowledge = {
 
 function resetDrawingKnowledge(){
 
-    drawingKnowledge = {
+    drawingKnowledge={
 
         skuMap:{},
 
@@ -43,10 +43,70 @@ function getDrawingKnowledge(){
 function learnDrawing(record){
 
     if(!record){
+
         return;
+
     }
 
-    // Will be implemented next
+    if(!record.categories){
+
+        return;
+
+    }
+
+    if(record.categories.length===0){
+
+        return;
+
+    }
+
+    // Use first selected category
+    const category =
+        record.categories[0];
+
+    const config =
+        CHECKLIST_CONFIG[category];
+
+    if(!config){
+
+        return;
+
+    }
+
+    const level =
+        config.drawingLevel;
+
+    if(level==="FULL_HOME"){
+
+        drawingKnowledge.skuMap[record.item]={
+
+            page:Number(record.drawingPage),
+
+            category
+
+        };
+
+    }
+
+    else if(level==="ROOM"){
+
+        drawingKnowledge.roomPageMap[record.room]={
+
+            page:Number(record.drawingPage),
+
+            category
+
+        };
+
+    }
+
+    console.log(
+
+        "Drawing Knowledge",
+
+        drawingKnowledge
+
+    );
 
 }
 
